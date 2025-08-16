95975b4202964bbb377f3715fb32aaa24f2d9ca9
Eduardo <dudulj15@gmail.com>
2025-08-15 23:34:57 -0300

    Refactor FastAPI application initialization to enhance modularity
    This commit refactors the FastAPI application setup by introducing a custom App class that encapsulates the initialization logic. The new structure improves modularity by delegating middleware and router registration to dedicated functions, promoting cleaner code organization. The application title and description are now dynamically set from the configuration, enhancing maintainability and scalability.

--- api/app/main.py
+++ api/app/main.py
@@ -1,28 +1,22 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
+from .middleware import register_middlewares
 from .routers import register_routers
 from .settings.config import settings
 
-app = FastAPI(
-    title="API",
-    description="API for the application",
+
+class App(FastAPI):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title = settings.PROJECT_NAME
+        self.description = "API for the application"
+        self.docs_url = "/docs"
+        self.redoc_url = "/redoc"
+        self.openapi_url = "/openapi.json"
+        register_middlewares(self)
+        register_routers(self)
+
+
+app = App(
     version="0.1.0",
-    contact={
-        "name": "API Support",
-        "url": "https://www.google.com",
-    },
-    docs_url="/docs",
-    redoc_url="/redoc",
-    openapi_url="/openapi.json",
 )
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=settings.ALLOWED_ORIGINS,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-register_routers(app)
